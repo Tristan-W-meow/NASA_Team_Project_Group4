4c08a44e7a003d8e5d0ee448bce91f6e3bac34d3
Tristan-W-meow <tw2mq@students.coconino.edu>
2025-10-11 15:21:05 -0700

    Added test lines

---
+++ NASA-Project-Human-4/NASA-Project-Human-4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NASA_Project_Human_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Mean world :(")
+            // Adding a test line / comment to test the repository
+        }
+    }
+}
